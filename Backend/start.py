66a8d0c227ca3c85cb850604d30afce18011d951
Brady Bates <brady.bates@my.uwrf.edu>
2023-02-13 14:51:50 -0600

    Test commit

--- Backend/start.py
+++ Backend/start.py
@@ -1,1 +1,13 @@
+from flask import Flask
+
 # Start
+print("hello world")
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index(): 
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=81)

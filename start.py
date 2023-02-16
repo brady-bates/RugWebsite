ce9fd4844398b45112e41ed1efbe6283e3f9ff2c
Brady Bates <brady.bates@my.uwrf.edu>
2023-02-16 16:50:50 -0600

    Testing Jinja2 HTML extension

--- start.py
+++ start.py
@@ -7,5 +7,17 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def home(): 
     return render_template("home.html")
 
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+@app.route("/tables")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/rugs")
+def rugs():
+    return render_template("rugs.html")
+
 if __name__ == "__main__":
     app.run(debug=True)

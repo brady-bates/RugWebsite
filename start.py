07fac4c908b0b7329ec8fc4b7961d9fd573d3c79
Brady Bates <brady.bates@my.uwrf.edu>
2023-02-16 11:58:37 -0600

    Jinja templates

--- start.py
+++ start.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 
-# Start
-print("hello world")
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/')
+@app.route("/")
+def home(): 
+    return render_template("home.html")
 
-def index(): 
-    return render_template('index.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)

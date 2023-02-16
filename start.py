0e4cca3ca3dbce52a18cd03209ac13fa9e79968e
Duncan Chou <duncan.chou@my.uwrf.edu>
2023-02-16 11:07:21 -0600

    Commit 3

---
+++ start.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
+
+# Start
+print("hello world")
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+
+def index(): 
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import flask
+from flask import Flask
 
 # Test File
 

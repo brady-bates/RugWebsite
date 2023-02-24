050251391e1ec009f38db4a5ca4e193555755c95
Brady Bates <brady.bates@my.uwrf.edu>
2023-02-24 11:24:59 -0600

    Got SQLite DB working

--- start.py
+++ start.py
@@ -2,7 +2,6 @@
 # TODO Add login system and page
 # TODO Add SQL Alchemy Database
 # TODO Add cart
-# TODO Add more necessary flask packages
 #
 # AFTER MEETING WITH ELIJAH
 # TODO Add products
@@ -14,33 +13,60 @@
 # TODO Figure out purchase verification
 # TODO Add to server
 
-from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_required, logout_user, UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import Form, FlaskForm
+from flask_wtf import FlaskForm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, length, ValidationError, Length
-from flask_bcrypt import Bcrypt
+from wtforms.validators import InputRequired, ValidationError, Length
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = "TOPSECRETKEY"
+app.secret_key = "TOPSECRETKEY"
+
+Base = declarative_base()
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-
 class User(db.Model, UserMixin):
-    userid   = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(15), nullable=False)
+    __tablename__ = "Users"
+
+    id       = db.Column(db.Integer, primary_key=True, nullable=True, unique=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(15), nullable=False)
+
+    def __init__(self, username, password):
+
+        self.username = username
+        self.password = password
+
+    # Python's toString()
+    def __repr__(self):
+        return f"{self.username}"
 
 
-with app.app_context():
-    print("hg")
-    db.create_all()
+engine = create_engine('sqlite:///database.db', echo=True)
+Base.metadata.create_all(engine)
 
+Session = sessionmaker(engine)
+session = Session()
+
+session.query(User).where(User.username.like("admin%")).delete()
+
+# session.delete(User).where.like()
+
+admin = User(username="admin", password="ADMIN_PASS")
+admin2 = User(username="admin2", password="ADMIN_PASS")
+session.add(admin)
+session.add(admin2)
+
+session.commit()
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
@@ -56,14 +82,12 @@ class RegisterForm(FlaskForm):
             )
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=  [InputRequired(),
+    username = StringField(validators  =[InputRequired(),
                                          Length(min=4, max=20)],
                                          render_kw={"placeholder": "Username"})
-
     password = PasswordField(validators=[InputRequired(),
                                          Length(min=4, max=20)],
                                          render_kw={"placeholder": "Password"})
-
     submit = SubmitField("Login")
 
 
@@ -95,14 +119,17 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("login"))
-
+        # If Username does not exist in DB
+        if session.query(session.query(User).filter_by(username="admin").exists()).scalar():
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("login"))
+        # If username exists in DB
+        else:
+            return f"Username already taken"
     return render_template("register.html", form=form)
 
 
@@ -130,6 +157,9 @@ def edit():
 @login_required
 @app.route("/user")
 def user():
+    if "User" in session:
+        User = session["User"]
+
     return render_template("user.html")
 
 
